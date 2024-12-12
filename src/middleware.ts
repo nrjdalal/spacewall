d7f43ea7d44839a8b7af072f3220121e9bd5ae49
Neeraj Dalal <admin@nrjdalal.com>
2024-12-12 22:22:27 +0530

    feat: save awesome state

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,22 +5,24 @@ const { auth } = NextAuth(authConfig)
 
 export default auth(async function middleware(req) {
   const { auth, nextUrl } = req
+  const { pathname, search, searchParams } = nextUrl
 
   const isAuthenticated = !!auth
 
-  if (/^\/(access|x)(\/|$)/.test(nextUrl.pathname)) {
-    if (!isAuthenticated && nextUrl.pathname !== "/access") {
-      const redirect = nextUrl.pathname + nextUrl.search
-      return Response.redirect(new URL(`/access?redirect=${redirect}`, nextUrl))
+  if (/^\/(access|x)(\/|$)/.test(pathname)) {
+    if (isAuthenticated && pathname === "/access") {
+      return redirect(searchParams.get("redirect") || "/x", nextUrl)
     }
-    if (isAuthenticated && nextUrl.pathname === "/access") {
-      return Response.redirect(
-        new URL(nextUrl.searchParams.get("redirect") || "/x", nextUrl),
-      )
+    if (!isAuthenticated && pathname !== "/access") {
+      return redirect(`/access?redirect=${pathname + search}`, nextUrl)
     }
   }
 })
 
 export const config = {
-  matcher: ["/access", "/x/:path*"],
+  matcher: ["/access/:path*", "/x/:path*"],
+}
+
+function redirect(url: string, base: URL) {
+  return Response.redirect(new URL(url, base))
 }
