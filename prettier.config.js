5a1d1d933bd8860552483b9b0e1f3104fae31ebd
Neeraj Dalal <admin@nrjdalal.com>
2024-12-12 16:37:34 +0530

    feat: added custom fonts

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+}
---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,33 @@
+import localFont from 'next/font/local'
+
+export const fontMono = localFont({
+  src: [
+    {
+      path: './JetBrainsMono-Variable.ttf',
+      style: 'normal',
+      weight: '100 800',
+    },
+    {
+      path: './JetBrainsMono-Italic.ttf',
+      style: 'italic',
+      weight: '100 800',
+    },
+  ],
+  variable: '--font-mono',
+})
+
+export const fontSans = localFont({
+  src: [
+    {
+      path: './DMSans-Variable.ttf',
+      style: 'normal',
+      weight: '100 900',
+    },
+    {
+      path: './DMSans-Italic.ttf',
+      style: 'italic',
+      weight: '100 900',
+    },
+  ],
+  variable: '--font-sans',
+})
