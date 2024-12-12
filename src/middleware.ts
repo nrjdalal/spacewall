82eb46eef8ff31b1019774e108661c7747b4785a
Neeraj Dalal <admin@nrjdalal.com>
2024-12-12 20:22:27 +0530

    feat: save awesome state

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,15 +8,13 @@ export default auth(async function middleware(req) {
 
   const isAuthenticated = !!auth
 
-  const PROTECTED_ROUTES = ["/access", "/x"]
-
-  if (PROTECTED_ROUTES.some((route) => nextUrl.pathname.startsWith(route))) {
+  // protect routes rules
+  if (["/access", "/x"].some((route) => nextUrl.pathname.startsWith(route))) {
     if (isAuthenticated && nextUrl.pathname === "/access") {
       return Response.redirect(
         new URL(nextUrl.searchParams.get("redirect") || "/x", nextUrl),
       )
     }
-
     if (!isAuthenticated && nextUrl.pathname !== "/access") {
       const redirect = nextUrl.pathname + nextUrl.search
       return Response.redirect(new URL(`/access?redirect=${redirect}`, nextUrl))
