1a51b8f994ce34b654963ab65d55ce1193af5e71
Neeraj Dalal <admin@nrjdalal.com>
2024-12-16 00:39:48 +0530

    feat: dark mode

--- src/components/sidebar/nav-mode-switcher.tsx
+++ src/components/sidebar/nav-mode-switcher.tsx
@@ -42,7 +42,7 @@ export function NavModeSwitcher({
             >
               <div
                 className={cn(
-                  "bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg",
+                  "bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md",
                   !open && "absolute left-0",
                 )}
               >
