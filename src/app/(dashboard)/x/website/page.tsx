8713cad433dcf40d10541fdcffe088dcd915c328
Neeraj Dalal <admin@nrjdalal.com>
2024-12-17 02:37:06 +0530

    feat: website placeholder

--- src/app/(dashboard)/x/website/page.tsx
+++ src/app/(dashboard)/x/website/page.tsx
@@ -28,10 +28,10 @@ export default async function Page() {
 
           <div>
             <div className="mt-8 flex items-center space-x-3 rounded-lg border p-3">
-              <div className="size-18 rounded-lg border"></div>
+              <div className="aspect-square size-18 rounded-lg border"></div>
               <div>
                 <h1 className="font-medium">#baliVlog</h1>
-                <p className="text-foreground/60 text-sm">
+                <p className="text-foreground/60 text-xs lg:text-sm">
                   A trip to Bali with my friends. It was amazing!
                 </p>
               </div>
