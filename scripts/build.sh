33b2a09257a64edddd55aaf28759b8e2ab4a2749
Neeraj Dalal <admin@nrjdalal.com>
2024-12-19 01:28:17 +0530

    feat: ui fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,12 +6,6 @@ DB_PUSH=false
 DEPLOYMENT_URL="https://vercel.com/nrjdalals-projects/spacewall/${VERCEL_DEPLOYMENT_ID#dpl_}"
 
 notify_failure() {
-  # curl -s \
-  #   --form-string "token=$PUSHOVER_TOKEN" \
-  #   --form-string "user=$PUSHOVER_USER" \
-  #   --form-string "html=1" \
-  #   --form-string "message=🔴 $VERCEL_ENV build failed, <a href=\"$DEPLOYMENT_URL\">check logs</a>" \
-  #   https://api.pushover.net/1/messages.json
   curl \
     -H "Title: Vercel" \
     -H "Priority: high" \
@@ -23,8 +17,6 @@ notify_failure() {
 }
 
 trap 'notify_failure' ERR
-
-false
 
 start_time=$(date +%s)
 
@@ -39,9 +31,11 @@ elapsed_time=$((end_time - start_time))
 minutes=$((elapsed_time / 60))
 seconds=$((elapsed_time % 60))
 
-curl -s \
-  --form-string "token=$PUSHOVER_TOKEN" \
-  --form-string "user=$PUSHOVER_USER" \
-  --form-string "priority=-1" \
-  --form-string "message=🟢 $VERCEL_ENV build completed in ${minutes}m ${seconds}s" \
-  https://api.pushover.net/1/messages.json
+curl \
+  -H "Title: Vercel" \
+  -H "Priority: low" \
+  -H "Tags: green_circle" \
+  -H "Click: $DEPLOYMENT_URL" \
+  -H "Actions: view, deployment logs, $DEPLOYMENT_URL" \
+  -d "$VERCEL_ENV build completed in ${minutes}m ${seconds}s" \
+  ntfy.sh/nrjdalal
