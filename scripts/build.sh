b198d450acb2735938a0471f8e018153c5df1419
Neeraj Dalal <admin@nrjdalal.com>
2024-12-19 01:48:30 +0530

    feat: ui fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,10 +9,9 @@ notify_failure() {
   curl \
     -H "Title: Vercel" \
     -H "Priority: high" \
-    -H "Tags: red_circle" \
     -H "Click: $DEPLOYMENT_URL" \
     -H "Actions: view, deployment logs, $DEPLOYMENT_URL" \
-    -d "$VERCEL_ENV build failed" \
+    -d "🔴 $VERCEL_ENV build failed" \
     ntfy.sh/nrjdalal
 }
 
@@ -34,8 +33,7 @@ seconds=$((elapsed_time % 60))
 curl \
   -H "Title: Vercel" \
   -H "Priority: low" \
-  -H "Tags: green_circle" \
   -H "Click: $DEPLOYMENT_URL" \
   -H "Actions: view, deployment logs, $DEPLOYMENT_URL" \
-  -d "$VERCEL_ENV build completed in ${minutes}m ${seconds}s" \
+  -d "🟢 $VERCEL_ENV build completed in ${minutes}m ${seconds}s" \
   ntfy.sh/nrjdalal
