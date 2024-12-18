143311e784fe286b89c0f08890ad5df7bad37910
Neeraj Dalal <admin@nrjdalal.com>
2024-12-19 00:29:55 +0530

    feat: ui fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ notify_failure() {
     --form-string "token=$PUSHOVER_TOKEN" \
     --form-string "user=$PUSHOVER_USER" \
     --form-string "html=1" \
-    --form-string "message=🔴 $VERCEL_ENV build failed, check <a href=\"$DEPLOYMENT_URL\"> deployment logs</a>" \
+    --form-string "message=🔴 $VERCEL_ENV build failed, <a href=\"$DEPLOYMENT_URL\">check logs</a>" \
     https://api.pushover.net/1/messages.json
 }
 
