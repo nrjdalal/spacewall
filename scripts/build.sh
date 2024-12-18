6963d5d4c6fa1bfbbfbf3bcaf92776e8aec45948
Neeraj Dalal <admin@nrjdalal.com>
2024-12-18 23:20:48 +0530

    feat: ui fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,22 +8,37 @@ notify_failure() {
   curl -s \
     --form-string "token=$PUSHOVER_TOKEN" \
     --form-string "user=$PUSHOVER_USER" \
-    --form-string "message=Build failed at $VERCEL_ENV env" \
+    --form-string "title=Error building $VERCEL_ENV" \
+    --form-string "message=Your last $VERCEL_ENV build failed." \
     https://api.pushover.net/1/messages.json
 }
 
 trap 'notify_failure' ERR
 
-false
+# Record the start time
+start_time=$(date +%s)
 
 if [ "$DB_PUSH" = true ]; then
   bun run drizzle-kit push
 fi
 
+# Build process
 bun run next build
 
+# Record the end time
+end_time=$(date +%s)
+
+# Calculate the duration
+elapsed_time=$((end_time - start_time))
+
+# Convert to minutes and seconds (optional)
+minutes=$((elapsed_time / 60))
+seconds=$((elapsed_time % 60))
+
+# Send success notification with build duration
 curl -s \
   --form-string "token=$PUSHOVER_TOKEN" \
   --form-string "user=$PUSHOVER_USER" \
-  --form-string "message=Build successful for $VERCEL_ENV env." \
+  --form-string "title=$VERCEL_ENV build is ready" \
+  --form-string "message=Build completed in $minutes minutes and $seconds seconds." \
   https://api.pushover.net/1/messages.json
