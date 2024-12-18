b79e7a44e742b28c3a5d3c6d73de49b2103944ce
Neeraj Dalal <admin@nrjdalal.com>
2024-12-18 23:05:53 +0530

    feat: ui fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,3 +9,9 @@ if [ "$DB_PUSH" = true ]; then
 fi
 
 bun run next build
+
+curl -s \
+  --form-string "token=$PUSHOVER_TOKEN" \
+  --form-string "user=$PUSHOVER_USER" \
+  --form-string "message=BUILD SUCCESSFUL" \
+  https://api.pushover.net/1/messages.json
