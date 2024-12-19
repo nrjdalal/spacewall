6e94731931d77504962be9d8424300155f4ff026
Neeraj Dalal <admin@nrjdalal.com>
2024-12-19 19:49:04 +0530

    feat: temp ui

--- src/app/(dashboard)/x/website/page.tsx
+++ src/app/(dashboard)/x/website/page.tsx
@@ -5,6 +5,14 @@
 import XHeader from "@/components/common/x-header"
 import { Button } from "@/components/ui/button"
 import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
   Dialog,
   DialogContent,
   DialogTitle,
@@ -19,6 +27,11 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 import { Textarea } from "@/components/ui/textarea"
 import WebsiteView from "@/components/views/website"
 import { Content, ContentPreview, ContentRoot } from "@/components/x/content"
@@ -26,7 +39,14 @@ import { cn, createChecksum } from "@/lib/utils"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import Compressor from "compressorjs"
-import { Edit, Image as ImageIcon, Loader2, Trash2 } from "lucide-react"
+import {
+  Check,
+  ChevronsUpDown,
+  Edit,
+  Image as ImageIcon,
+  Loader2,
+  Trash2,
+} from "lucide-react"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { toast } from "sonner"
@@ -468,8 +488,15 @@ const AddWidget = ({ id }: { id: string }) => {
 
   const queryClient = useQueryClient()
 
+  const types = [
+    {
+      label: "Link",
+      value: "link",
+    },
+  ]
+
   const formSchema = z.object({
-    type: z.enum(["link"]),
+    type: z.string(),
     data: z.object({
       title: z.string().min(1),
       url: z.string().min(1),
@@ -484,7 +511,7 @@ const AddWidget = ({ id }: { id: string }) => {
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      type: "link" as const,
+      type: "link",
       data: {
         title: "",
         url: "",
@@ -603,17 +630,58 @@ const AddWidget = ({ id }: { id: string }) => {
                 control={form.control}
                 name="type"
                 render={({ field }) => (
-                  <FormItem className="relative hidden">
-                    <FormLabel>Type</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="mt-1.5"
-                        placeholder={form.getValues("type")}
-                        {...field}
-                        readOnly
-                      />
-                    </FormControl>
-                    <FormMessage className="absolute -bottom-5 text-xs" />
+                  <FormItem className="flex flex-col">
+                    <FormLabel>Widget</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant="outline"
+                            role="combobox"
+                            className={cn(
+                              "mt-1.5 justify-between pl-3",
+                              !field.value && "text-muted-foreground",
+                            )}
+                          >
+                            {field.value
+                              ? types.find((type) => type.value === field.value)
+                                  ?.label
+                              : "Select widget"}
+                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="-mt-px p-0" align="end">
+                        <Command>
+                          <CommandInput placeholder="Search widgets" />
+                          <CommandList>
+                            <CommandEmpty>No widget found.</CommandEmpty>
+                            <CommandGroup>
+                              {types.map((type) => (
+                                <CommandItem
+                                  value={type.label}
+                                  key={type.value}
+                                  onSelect={() => {
+                                    form.setValue("type", type.value)
+                                  }}
+                                >
+                                  {type.label}
+                                  <Check
+                                    className={cn(
+                                      "ml-auto",
+                                      type.value === field.value
+                                        ? "opacity-100"
+                                        : "opacity-0",
+                                    )}
+                                  />
+                                </CommandItem>
+                              ))}
+                            </CommandGroup>
+                          </CommandList>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
@@ -621,24 +689,6 @@ const AddWidget = ({ id }: { id: string }) => {
 
             {form.getValues("type") === "link" && (
               <>
-                <FormField
-                  control={form.control}
-                  name="data.image"
-                  render={() => (
-                    <FormItem className="relative">
-                      <FormLabel>Image</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="file"
-                          accept="image/jpeg,image/png"
-                          className="mt-1.5 pt-1.5"
-                          {...form.register("data.image")}
-                        />
-                      </FormControl>
-                      <FormMessage className="absolute -bottom-5 text-xs" />
-                    </FormItem>
-                  )}
-                />
                 <FormField
                   control={form.control}
                   name="data.title"
@@ -649,6 +699,23 @@ const AddWidget = ({ id }: { id: string }) => {
                         <Input
                           className="mt-1.5"
                           placeholder="Title"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="absolute -bottom-5 text-xs" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="data.url"
+                  render={({ field }) => (
+                    <FormItem className="relative">
+                      <FormLabel>URL</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="mt-1.5"
+                          placeholder="URL"
                           {...field}
                         />
                       </FormControl>
@@ -675,15 +742,16 @@ const AddWidget = ({ id }: { id: string }) => {
                 />
                 <FormField
                   control={form.control}
-                  name="data.url"
-                  render={({ field }) => (
+                  name="data.image"
+                  render={() => (
                     <FormItem className="relative">
-                      <FormLabel>URL</FormLabel>
+                      <FormLabel>Image</FormLabel>
                       <FormControl>
                         <Input
-                          className="mt-1.5"
-                          placeholder="URL"
-                          {...field}
+                          type="file"
+                          accept="image/jpeg,image/png"
+                          className="mt-1.5 pt-1.5"
+                          {...form.register("data.image")}
                         />
                       </FormControl>
                       <FormMessage className="absolute -bottom-5 text-xs" />
