266d2e01b0c3591da29da5118882bbd263164e27
Neeraj Dalal <admin@nrjdalal.com>
2024-12-25 01:16:18 +0530

    feat: ui enhancements

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button"
 import WebsiteView from "@/components/views/website"
 import { db, websiteBlocks, websites } from "@/db"
 import { and, eq } from "drizzle-orm"
+import { ChevronRight, Crown } from "lucide-react"
+import Link from "next/link"
 
 export default async function Page({
   params,
@@ -15,7 +18,38 @@ export default async function Page({
     .where(and(eq(websites.slug, slug)))
 
   if (website.length === 0) {
-    return <div>Website not found. Please try later.</div>
+    return (
+      <main className="flex min-h-dvh items-center justify-center p-7.5">
+        <div className="max-w-sm space-y-6 text-center">
+          <h1 className="flex items-center justify-center text-6xl font-medium">
+            Space
+            <Crown className="size-13" />
+            all
+          </h1>
+
+          <p className="animate-bounce text-lg font-medium text-pretty uppercase italic">
+            Claim your space
+          </p>
+
+          <div className="flex items-center justify-center gap-x-5 px-5">
+            <Link href="/access" className="w-full">
+              <Button className="h-10 w-full">
+                <span className="mt-0.75">Log in</span>
+                <ChevronRight />
+              </Button>
+            </Link>
+            <Link href="mailto:admin@nrjdalal.com" className="w-full">
+              <Button variant="outline" className="h-10 w-full">
+                <span className="mt-0.75">Contact</span>
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <p className="text-primary/35 absolute bottom-4 text-center text-xs">
+          Current version is for demonstration purposes only.
+        </p>
+      </main>
+    )
   }
 
   const blocks = await db
@@ -49,7 +83,7 @@ export default async function Page({
   )
 
   // @ts-expect-error - TS doesn't know about the order property
-  data.sortedBlocks = data.order.map((orderItem: OrderItem) => ({
+  data.sortedBlocks = data.order?.map((orderItem: OrderItem) => ({
     ...(blocksMap.get(orderItem.id) || {}),
   }))
   // .filter((orderItem: OrderItem) => orderItem.active)
