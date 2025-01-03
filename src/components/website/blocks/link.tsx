d9b2978202dbbe05e7f71604eabbeafa24b3d887
Neeraj Dalal <admin@nrjdalal.com>
2025-01-03 23:09:00 +0530

    feat: dotted ones are not active blocks

--- src/components/website/blocks/link.tsx
+++ src/components/website/blocks/link.tsx
@@ -2,6 +2,7 @@
 
 import { Icons } from "@/assets/icons"
 import { BlockContent, BlockEditor } from "@/components/website/block-manager"
+import { cn } from "@/lib/utils"
 import {
   SiBluesky,
   SiFacebook,
@@ -20,16 +21,6 @@ interface Props {
   id: string
   type: string
   active: boolean
-  meta?: {
-    title?: string
-    href?: string
-    image?: string
-    description?: string
-  }
-}
-
-interface ViewLink {
-  id: string
   meta?: {
     title?: string
     href?: string
@@ -73,13 +64,13 @@ export const BlockLink = (props: Props) => {
   })
 
   return (
-    <BlockContent id={props.id}>
+    <BlockContent id={props.id} active={props.active}>
       <BlockEditor name="Link Block" schema={schema} data={props} />
     </BlockContent>
   )
 }
 
-export const ViewLink = (props: ViewLink) => {
+export const ViewLink = (props: Props) => {
   const Logos = {
     "bsky.app": SiBluesky,
     "facebook.com": SiFacebook,
@@ -93,56 +84,69 @@ export const ViewLink = (props: ViewLink) => {
   }
 
   return (
-    <Link
-      key={props.id}
-      href={props.meta?.href || "/x/website"}
-      target={props.meta?.href ? "_blank" : "_self"}
-      className="bg-sidebar relative grid grid-cols-6 items-center gap-1.5 rounded-md border p-1"
-    >
-      <div className="col-span-1">
-        {props.meta?.image ? (
-          <div className="bg-secondary aspect-square h-full max-h-14 overflow-hidden rounded-md border">
-            <img
-              className="aspect-square w-full object-cover object-center"
-              src={
-                props.meta?.image.startsWith("data:")
-                  ? props.meta?.image
-                  : process.env.NEXT_PUBLIC_CDN_URL + "/" + props.meta?.image
-              }
-              alt={props.meta?.title}
-            />
-          </div>
-        ) : (
-          <div className="grid aspect-square h-full max-h-14 place-content-center">
-            {Object.keys(Logos).some((key) =>
-              new URL(
-                props.meta?.href || "https://spacewall.me",
-              ).hostname.includes(key),
-            ) ? (
-              createElement(
-                Logos[
-                  Object.keys(Logos).find(
-                    (key) => props.meta?.href?.includes(key) ?? false,
-                  ) as keyof typeof Logos
-                ],
-                { className: "size-8" },
-              )
+    <>
+      {props.meta?.href ? (
+        <Link
+          key={props.id}
+          href={props.meta?.href || "/x/website"}
+          target={props.meta?.href ? "_blank" : "_self"}
+          className={cn(
+            "bg-sidebar relative grid grid-cols-6 items-center gap-1.5 rounded-md border p-1",
+            !props.active && "border-dashed",
+          )}
+        >
+          <div className="col-span-1">
+            {props.meta?.image ? (
+              <div className="bg-secondary aspect-square h-full max-h-14 overflow-hidden rounded-md border">
+                <img
+                  className="aspect-square w-full object-cover object-center"
+                  src={
+                    props.meta?.image.startsWith("data:")
+                      ? props.meta?.image
+                      : process.env.NEXT_PUBLIC_CDN_URL +
+                        "/" +
+                        props.meta?.image
+                  }
+                  alt={props.meta?.title}
+                />
+              </div>
             ) : (
-              <LinkIcon className="text-muted-foreground size-6 stroke-1" />
+              <div className="grid aspect-square h-full max-h-14 place-content-center">
+                {Object.keys(Logos).some((key) =>
+                  new URL(
+                    props.meta?.href || "https://spacewall.me",
+                  ).hostname.includes(key),
+                ) ? (
+                  createElement(
+                    Logos[
+                      Object.keys(Logos).find(
+                        (key) => props.meta?.href?.includes(key) ?? false,
+                      ) as keyof typeof Logos
+                    ],
+                    { className: "size-8" },
+                  )
+                ) : (
+                  <LinkIcon className="text-muted-foreground/50 size-6 stroke-1" />
+                )}
+              </div>
             )}
           </div>
-        )}
-      </div>
-      <div className="col-span-4 w-full text-center">
-        <h1 className="line-clamp-2 text-sm font-medium break-words">
-          {props.meta?.title || "Link Block"}
-        </h1>
-        {props.meta?.description && (
-          <p className="text-muted-foreground text-xs">
-            {props.meta?.description}
-          </p>
-        )}
-      </div>
-    </Link>
+          <div className="col-span-4 w-full text-center">
+            <h1 className="line-clamp-2 text-sm font-medium break-words">
+              {props.meta?.title || "Link Block"}
+            </h1>
+            {props.meta?.description && (
+              <p className="text-muted-foreground text-xs">
+                {props.meta?.description}
+              </p>
+            )}
+          </div>
+        </Link>
+      ) : (
+        <div className="text-muted-foreground flex h-14 items-center justify-center rounded-md border border-dashed text-sm">
+          Link Block
+        </div>
+      )}
+    </>
   )
 }
