3df890a6b2ee345b8516d00d5b81fb0e9d1b9959
Neeraj Dalal <admin@nrjdalal.com>
2025-01-04 00:55:40 +0530

    feat: grouped socials

--- src/components/website/blocks/social.tsx
+++ src/components/website/blocks/social.tsx
@@ -1,4 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { Icons } from "@/assets/icons"
 import { BlockContent, BlockEditor } from "@/components/website/block-manager"
+import {
+  SiBluesky,
+  SiFacebook,
+  SiGmail,
+  SiInstagram,
+  SiNpm,
+  SiYoutube,
+} from "@icons-pack/react-simple-icons"
+import { LinkIcon } from "lucide-react"
+import Link from "next/link"
+import { createElement } from "react"
 import { z } from "zod"
 
 interface Props {
@@ -39,13 +53,56 @@ const Block = (props: Props) => {
 }
 
 const View = (props: Props) => {
+  const Logos = {
+    "bsky.app": SiBluesky,
+    "facebook.com": SiFacebook,
+    "github.com": Icons.Github,
+    "gmail.com": SiGmail,
+    "instagram.com": SiInstagram,
+    "npmjs.com": SiNpm,
+    "x.com": Icons.X,
+    "youtube.com": SiYoutube,
+    "linkedin.com": Icons.Linkedin,
+  }
+
   return (
     <>
       {props.meta?.href ? (
-        <div>Temp</div>
+        <Link href={props.meta.href} target="_blank">
+          {props.meta?.logo ? (
+            <img
+              className="h-8 w-8 rounded-full object-cover object-center"
+              src={
+                props.meta.logo.startsWith("data:")
+                  ? props.meta.logo
+                  : process.env.NEXT_PUBLIC_CDN_URL + "/" + props.meta.logo
+              }
+              alt={props.meta.href}
+            />
+          ) : (
+            <>
+              {Object.keys(Logos).some((key) =>
+                new URL(
+                  props.meta?.href || "https://spacewall.me",
+                ).hostname.includes(key),
+              ) ? (
+                createElement(
+                  Logos[
+                    Object.keys(Logos).find(
+                      (key) => props.meta?.href?.includes(key) ?? false,
+                    ) as keyof typeof Logos
+                  ],
+                  { className: "size-6" },
+                )
+              ) : (
+                <LinkIcon className="text-muted-foreground/50 size-6 stroke-1" />
+              )}
+            </>
+          )}
+        </Link>
       ) : (
-        <div className="text-muted-foreground flex h-14 items-center justify-center rounded-md border border-dashed text-sm">
-          Social Block
+        <div className="flex size-6 items-center justify-center rounded-md border border-dashed">
+          ?
         </div>
       )}
     </>
