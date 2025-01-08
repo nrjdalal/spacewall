2b0b9418a1dc10de7b1354f5aa33297693b07e78
Neeraj Dalal <admin@nrjdalal.com>
2025-01-09 00:48:04 +0530

    test: temp payment page

--- src/app/(demo)/d/payment-page/page.tsx
+++ src/app/(demo)/d/payment-page/page.tsx
@@ -12,7 +12,7 @@ import { z } from "zod"
 export default function Page() {
   return (
     <div className="mx-auto grid max-w-screen-xl grid-cols-1 p-5 pt-0 lg:grid-cols-12">
-      <div className="mr-5 space-y-5 p-5 md:col-span-7">
+      <div className="mr-5 space-y-5 p-5 lg:col-span-8">
         {/* header */}
         <div className="flex w-full items-center gap-2">
           <SiApple className="size-8" />
@@ -74,27 +74,28 @@ export default function Page() {
         </div>
       </div>
       {/* payment info */}
-      <div className="bg-sidebar mt-48 h-min w-full max-w-96 space-y-10 rounded-md border p-5 md:col-span-4">
-        <h1 className="font-medium">Payment Details</h1>
-
-        <ZodHookForm
-          schema={z.object({
-            fullname: z.string().field({
-              placeholder: "Full Name",
-              default: "",
-            }),
-            email: z.string().field({
-              placeholder: "Email",
-              default: "",
-            }),
-            phone: z.string().field({
-              placeholder: " Phone",
-              prefix: "+91",
-              default: "",
-            }),
-          })}
-          submitText="Pay $999"
-        />
+      <div className="flex h-min justify-center lg:col-span-4 lg:mt-48">
+        <div className="bg-sidebar w-full max-w-96 space-y-10 rounded-md border p-5">
+          <h1 className="font-medium">Payment Details</h1>
+          <ZodHookForm
+            schema={z.object({
+              fullname: z.string().field({
+                placeholder: "Full Name",
+                default: "",
+              }),
+              email: z.string().field({
+                placeholder: "Email",
+                default: "",
+              }),
+              phone: z.string().field({
+                placeholder: " Phone",
+                prefix: "+91",
+                default: "",
+              }),
+            })}
+            submitText="Pay $999"
+          />
+        </div>
       </div>
     </div>
   )
