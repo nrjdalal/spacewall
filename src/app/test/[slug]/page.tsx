73f1a1c749269233a4494472b64067afa202653e
Neeraj Dalal <admin@nrjdalal.com>
2025-01-09 19:39:22 +0530

    test: revalidation for pages

--- src/app/test/[slug]/page.tsx
+++ src/app/test/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { db, paymentPages } from "@/db"
 import { eq } from "drizzle-orm"
 
-export const revalidate = 60 * 5
+export const revalidate = 60
 export const dynamicParams = true
 
 export default async function Page({
