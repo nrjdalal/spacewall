a7b1400745fff190402d86443c44bf678662040d
Neeraj Dalal <admin@nrjdalal.com>
2025-04-28 06:12:00 +0530

    chore: tweaks > next.config.ts
    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/access",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
